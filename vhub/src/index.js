fdba7a3fc352341a06472e5674b56b7464239446
Hemanth anvesh Charugundla <anvesh@Hemanths-MacBook-Pro.local>
2019-07-18 17:03:49 -0500

    [R+M]Code

--- vhub/src/index.js
+++ vhub/src/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Routing from "./views/routes/route";
+import {createStore} from "redux";
+import vendorApp from './reducers/reducers';
+import { Provider } from "react-redux";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(vendorApp);
+
+const APP = (
+    <Provider store={store}>
+        <Routing/>
+    </Provider>
+);
+
+ReactDOM.render(APP, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
