64eb21548a348510e13008a82d79dac45d6b08e9
Caleb Reiter <caleb@reiters.org>
2019-07-26 13:53:01 -0500

    merge master into branch

--- vhub/src/views/routes/route.js
+++ vhub/src/views/routes/route.js
@@ -4,11 +4,8 @@ import Home from "../home/home-connector";
 import LoginPage from "../login/login-connector";
 import Header from '../common/header';
 import RequestList from "../request-list/request-list-connector";
-<<<<<<< HEAD
 import Proposals from "../proposals/proposals-connector";
-=======
 import Request from '../request/request-connector';
->>>>>>> master
 
 function Routing(){
     return (
@@ -18,12 +15,9 @@ function Routing(){
             <Route path="/home" component={Home}/>
             <Route path="/" exact component={LoginPage}/>
             <Route path="/request-list" component={RequestList}/>
-<<<<<<< HEAD
             <Route path="/proposals" component={Proposals}/>
-=======
             <Route path='/request' exact component={Request}/>
             <Route path='/request/:id' component={Request}/>
->>>>>>> master
         </Router>
         </div>
     )
