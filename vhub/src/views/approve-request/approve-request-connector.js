c2b3086c826b25da2a3ea51a7fcad9eb06b945b4
Lhito Camson <lhito.camson@drake.edu>
2019-07-30 10:47:09 -0500

    added most of the functionality to approve

--- vhub/src/views/approve-request/approve-request-connector.js
+++ vhub/src/views/approve-request/approve-request-connector.js
@@ -1,25 +1,18 @@
 import {connect} from 'react-redux';
 import approveRequest from './approve-request';
+import {fetchRequestEnvelope} from '../../action-creators/request-envelopes';
 
-function mapStateToProps(state,ownProps){
-    // const interviewers = [
-    //     {key:'1', text:'Seymore Butts', value:'Seymore Butts'},
-    //     {key:'2', text:'Gene Vagine', value:'Gene Vagine'},
-    //     {key:'3', text:'Rick Sanchez', value:'Rick Sanchez'}
-    // ]
-    // const approvers = [
-    //     {key:'1', text:'Seymore Butts', value:'Seymore Butts'},
-    //     {key:'2', text:'Gene Vagine', value:'Gene Vagine'},
-    //     {key:'3', text:'Rick Sanchez', value:'Rick Sanchez'}
-    // ]
-    const request = state.requestLists.find(({id}) => {
-        return id === Number(ownProps.match.params.id)
-    })
+function mapStateToProps({requestEnvelope},ownProps){
+
     return{
-        request: state.request
+        requestEnvelope: requestEnvelope.item
     }
     
 }
 
+const mapDispatchToProps = {
+    fetchRequestEnvelope
+}
 
-export default connect(mapStateToProps)(approveRequest);
+
+export default connect(mapStateToProps,mapDispatchToProps)(approveRequest);
--- vhub/src/views/approve-request/approve-request.js
+++ vhub/src/views/approve-request/approve-request.js
@@ -1,51 +1,59 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {Form, Grid, Button, FormGroup} from 'semantic-ui-react';
+import {Form, Grid, Button, FormGroup, Loader} from 'semantic-ui-react';
 import ResourceForm from '../request/resourceForm-connector';
+import {Redirect} from 'react-router-dom';
 
 class ApproveRequestForm extends React.Component{
     constructor(props){
         super(props)
+
+        this.state={
+            requestEnvelope: this.props.requestEnvelope,
+            shouldRedirect: false,
+        }
     }
 
-componentWillMount(){
-    
-
+async componentWillMount(){
+   await this.props.fetchRequestEnvelope(Number(this.props.match.params.id))
+   this.setState({shouldRedirect:true});
 }
     
     render(){
 
-        console.log(this.props.request);
-        // console.log(this.state.request);
+        if(!this.props.requestEnvelope && this.state.shouldRedirect){
+            return <Redirect to='/request-list'/>
+        } else if(!this.props.requestEnvelope) {
+            return <Loader />
+        }
 
-        const resources = this.props.request.requestedResources.map((resource)=>{
-            return <ResourceForm key={resource.index} id={resource.index}/>
+        console.log(this.props.requestEnvelope);
+
+        const resources = this.props.requestEnvelope.resourceRequests.map((resource, index)=>{
+            return <ResourceForm key={index} id={index}/>
         })
         return(
             <Grid columns='16' centered>
                 <Grid.Column width='10'>
                 <Form>
                     <Form.Group widths='equal'>
-                            <Form.Input fluid label='Requested By' placeholder='Requested By'  readOnly/>
-                            <Form.Input fluid label='Requested Date' placeholder='Requested Date'  readOnly/>
+                            <Form.Input fluid label='Requested By' placeholder='Requested By' value={`${this.props.requestEnvelope.requester.firstName} ${this.props.requestEnvelope.requester.lastName}`} readOnly/>
+                            <Form.Input fluid label='Requested Date' placeholder='Requested Date' value={this.props.requestEnvelope.requestDate}  readOnly/>
                     </Form.Group>
                     <Form.Group widths='equal'>
-                        <Form.Select fluid label='Interviewer' options={this.props.interviewers} placeholder='Select'/>
-                        <Form.Select fluid label='Approvers' options={this.props.approvers} placeholder='Select'/>
+                        <Form.Select fluid label='Interviewer' options={this.props.interviewers} placeholder='Select' value={`${this.props.requestEnvelope.interviewer.firstName} ${this.props.requestEnvelope.interviewer.lastName}`}/>
+                        <Form.Select fluid label='Approvers' options={this.props.requestEnvelope.approvers.map(({email}) => email)} placeholder='Select'/>
                     </Form.Group>
-                        <Form.TextArea label='Business Case' placeholder='Describe Business Case' rows='6'/>
+                        <Form.TextArea label='Business Case' placeholder='Describe Business Case' value={this.props.requestEnvelope.businessCase} rows='6' readOnly/>
                     <h4>Client Info</h4>
                     <Form.Group widths='equal'>
-                        <Form.Input fluid label='Client Name' placeholder='Client Name' />
-                        <Form.Input fluid label='Team' placeholder='Team'/>
-                        <Form.Input fluid label='Manager' placeholder='Manager' />
+                        <Form.Input fluid label='Client Name' placeholder='Client Name' value={this.props.requestEnvelope.clientName} readOnly/>
+                        <Form.Input fluid label='Team' placeholder='Team' value={this.props.requestEnvelope.team} readOnly/>
+                        <Form.Input fluid label='Manager' placeholder='Manager' value={this.props.requestEnvelope.manager} readOnly/>
                     </Form.Group>
                     <Form.Group>
-                        <Form.Input fluid label='Location Preference' placeholder='Location'/>
+                        <Form.Input fluid label='Location Preference' placeholder='Location' value={this.props.requestEnvelope.locationPreference.stateOrProvince} readOnly/>
                     </Form.Group>
-                    {typeof this.props.defaultRequest === "undefined" && 
-                        <Button icon='add' label='Add Resource' onClick={this.props.addResourceRequest}/>
-                    }
                     
                     {resources}
                         <FormGroup widths='equal'>
